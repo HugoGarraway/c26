c9e68accbdd709220081693352efe13dafa539f1
Hugo Garraway <hugogarraway@Hugos-MacBook-Pro.local>
2021-06-02 14:07:29 +1000

    learningitbash

--- sketch.js
+++ sketch.js
@@ -24,6 +24,8 @@ function setup(){
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
 
+    box6 = new Box(12,34,56,78);
+
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
@@ -58,6 +60,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+
+    box6.display();
 
     bird.display();
     platform.display();
